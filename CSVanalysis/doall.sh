13a4578ded76c70ed907e59d3c128e2006f06c29
Jiri Kvita <jiri.kvita@cern.ch>
2020-03-03 20:38:39 +0100

    dvl

--- CSVanalysis/doall.sh
+++ CSVanalysis/doall.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 
-for file in DotaznikSummary ; do
+for file in DotaznikSummary DotaznikSummary_En ; do
   pdflatex ${file}.tex
   #cslatex ${file}.tex
   #dvips ${file}.dvi -o ${file}.ps
