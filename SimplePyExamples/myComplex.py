8f5d1903786c0ac2689515c44d5c86cda5971412
Jiri Kvita <jiri.kvita@gmail.com>
2022-04-27 09:44:46 +0200

    clpx updt

--- SimplePyExamples/myComplex.py
+++ SimplePyExamples/myComplex.py
@@ -5,14 +5,33 @@
 # https://www.geeksforgeeks.org/operator-overloading-in-python/
 # https://www.delftstack.com/howto/python/print-object-python/
 
-from math import pow, sqrt, atan, sin, cos
+from math import pow, sqrt, atan, sin, cos, atan2
 
+##################################################
+##################################################
+##################################################
+
+# jk 27.4.2022
 
 class cplx:
+    
     def __init__(self, re, im):
+        self.set(re,im)
+        self.compPolar()
+
+    def compPolar(self):
+        self.phi = atan2(self.im, self.re)
+        self.r = self.mag()
+    def set(self, re,im):
         self.re = re
         self.im = im
+        self.compPolar()
+    def setPolar(self, phi,r):
+        self.phi = phi
+        self.r = r
+        self.set(r*cos(phi), r*sin(phi))
 
+    # ---------------------------------------------------
     def __add__(self, right):
         return cplx(self.re + right.re, self.im + right.im)
     def __neg__(self):
@@ -24,6 +43,7 @@ class cplx:
     def __mul__(self, right):
         return cplx(self.re*right.re - self.im*right.im, self.re*right.im + self.im*right.re)
 
+    # ---------------------------------------------------
     def mag(self):
         val = pow(self.im,2) + pow(self.re,2)
         if val > 0.:
@@ -34,9 +54,10 @@ class cplx:
         if val > 0.:
             return cplx( (self.re*right.re + self.im*right.im) / val, (-self.re*right.im + self.im*right.re) / val)
         return cplx(0.,0.)
-
+    
+    # ---------------------------------------------------
     def phi(self):
-        return atan(re,im)
+        return atan2(self.im, self.re)
     def r(self):
         return self.mag()
     def conjug(self):
@@ -44,6 +65,7 @@ class cplx:
     def getconjug(self):
         return cplx(self.re, -self.im)
 
+##################################################
 
 c1 = cplx(1., 2.)
 c2 = cplx(-3., 1.)
@@ -57,4 +79,5 @@ cc2 = c2.getconjug()
 print(c1,c2)
 print(c1*cc1)
 print(c2*cc2)
-
+print(c1.phi, c2.phi)
+print(cc1.phi, cc2.phi)
