dc05b48d46d294884c341c92705c7676e62e7df2
Jiri Kvita <jiri.kvita@gmail.com>
2023-05-16 23:11:42 +0200

    Julia devel

--- Mandelbrot/mandelbrot.py
+++ Mandelbrot/mandelbrot.py
@@ -3,11 +3,14 @@
 
 # https://en.wikipedia.org/wiki/Mandelbrot_set
 # https://mathworld.wolfram.com/MandelbrotSet.html
+# https://en.wikipedia.org/wiki/Julia_set
 
-from math import pow, sqrt
+from math import pow, sqrt, pi, sin, cos
 import ROOT
 
 kMin = 1.e-3
+kMandel = 'Mandel'
+kJulia = 'Julia'
 
 stuff = []
 
@@ -32,6 +35,9 @@ class MyComplex:
     def __add__(self,other):
         result = MyComplex(self.re + other.re, self.im + other.im)
         return result
+    def __truediv__(self,other):
+        result = MyComplex( (self.re*other.re +  self.im*other.im)/ other.mag(),  (self.im*other.re + self.re*other.im) / other.mag())
+        return result
     def __mul__(self,other):
         result = MyComplex(self.re*other.re - self.im*other.im, self.re*other.im + self.im*other.re)
         return result
@@ -43,22 +49,34 @@ class MyComplex:
 ##################################################
 ##################################################
     
-def getNext(z, c):
-    znew = z*z + c
+def getNext(z, c, maintag):
+    # Mandelbrot:
+    znew = c
+    if maintag == kMandel:
+        znew = z*z + c
+    elif maintag == kJulia:
+        # some Julia:
+        alpha = pi*2./4.
+        xi = 0.7885
+        znew = z*z + MyComplex(xi*cos(alpha), xi*sin(alpha))
+        
+        #two = MyComplex(2.,0.)
+        #three = MyComplex(3.,0.)
+        #znew = (z*z + MyComplex(cos(alpha), sin(alpha))) / (two*z)
+        #znew = (z*z*z + MyComplex(cos(alpha), sin(alpha))) / (three*z*z)
+        
     #znew.print()
     return znew
 
 ##################################################
 
-def iterate(c, Nmax = 200, bound = 100.):
+def iterate(c, maintag, Nmax = 200, bound = 100.):
     n = 0
-    #z = c*1.
-    #z = c
     c1 = MyComplex(1.,0.)
     z = c*c1
     bounded = True
     while n < Nmax:
-        znew = getNext(z, c)
+        znew = getNext(z, c, maintag)
         if znew.mag() > bound:
             bounded = False
             break
@@ -68,13 +86,13 @@ def iterate(c, Nmax = 200, bound = 100.):
 
 ##################################################
 
-def Plot(nbx, x1, x2, nby, y1, y2, results):
+def Plot(nbx, x1, x2, nby, y1, y2, results, maintag, addtag):
     ROOT.gStyle.SetPadTopMargin(0.)
     ROOT.gStyle.SetPadBottomMargin(0.)
     ROOT.gStyle.SetPadRightMargin(0.)
     ROOT.gStyle.SetPadLeftMargin(0.)
 
-    name = 'Mandelbrot'
+    name = maintag
     title = name
     h2 = ROOT.TH2D(name, title, nbx, x1, x2, nby, y1, y2)
     bwx = (x2 - x1) / nbx
@@ -83,6 +101,8 @@ def Plot(nbx, x1, x2, nby, y1, y2, results):
         for j in range(0, len(results[i])):
             h2.SetBinContent(i+1, j+1, results[i][j])
     h2.Scale(1.)
+    h2.GetXaxis().SetTickSize(0.)
+    h2.GetYaxis().SetTickSize(0.)
     canname = name
     can = ROOT.TCanvas(canname, canname, 0, 0, 1200,1200)
     h2.SetStats(0)
@@ -102,9 +122,9 @@ def Plot(nbx, x1, x2, nby, y1, y2, results):
     for key,palette in palettes.items():
         ROOT.gStyle.SetPalette(palette)
         ROOT.gPad.SetLogz(0)
-        can.Print(can.GetName() + f'_{key}_liny.png')
+        can.Print(can.GetName() + f'_{key}{addtag}_liny.png')
         ROOT.gPad.SetLogz(1)
-        can.Print(can.GetName() + f'_{key}_logy.png')
+        can.Print(can.GetName() + f'_{key}{addtag}_logy.png')
 
     
     return can,h2
@@ -114,10 +134,26 @@ def Plot(nbx, x1, x2, nby, y1, y2, results):
 ##################################################
 
 
-
+# DEFAULT:
 ym = 1.1
 x1, x2 = -1.55, 0.55
 y1, y2 = -ym,ym
+maintag = kMandel
+addtag = ''
+
+# ZOOM:
+#ym = 1.1 / 3.
+#x1, x2 = -1.45, -0.65
+#y1, y2 = -ym,ym
+#addtag = '_zoom1'
+
+# Julia
+ym = 1.5
+x1, x2 = -ym,ym
+y1, y2 = -ym,ym
+maintag = kJulia
+addtag = ''
+
 
 nn = 2000
 nbx = nn
@@ -127,7 +163,7 @@ bwy = (y2 - y1) / nby
 
 results = []
 
-verb = 100
+verb = 2000
 verbose = False
 
 # bin centers bcx, bcy
@@ -144,7 +180,7 @@ for i in range(0, nbx):
         c = MyComplex(bcx, bcy)
         if verbose:
             print('Iterating...')
-        zn,bounded,n = iterate(c)
+        zn,bounded,n = iterate(c, maintag)
         if bounded:
             results[-1].append(kMin)
         else:
@@ -153,7 +189,7 @@ for i in range(0, nbx):
             print('  ...result: mag={:1.3f} n={}'.format(zn.mag(), n))
 
 #print(results)
-can, h2 = Plot(nbx, x1, x2, nby, y1, y2, results)
+can, h2 = Plot(nbx, x1, x2, nby, y1, y2, results, maintag, addtag)
 stuff.append([can,h2])
 
 
